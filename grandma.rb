04404bdd397de2469262b2cb04e7fadf42206b36
Philip Sterling <philipssterlings@gmail.com>
2019-05-31 20:20:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,5 +17,5 @@ def speak_to_grandma(input)
   elsif(input != "I LOVE YOU GRANDMA!")
     puts "NO, NOT SINCE 1938!"
   else
-    puts "I LOVE YOU TOO, PUMPKIN!"
+    puts "I LOVE YOU TOO PUMPKIN!"
 end
